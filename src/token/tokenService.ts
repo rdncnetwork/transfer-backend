a05e8821578c7cee384e92498008e2d438aedf1c
Tushar Dixit <167959110+tusharXrouter@users.noreply.github.com>
2025-02-10 00:12:35 +0530

    Enhance error handling and logging in token transfer service

--- src/token/tokenService.ts
+++ src/token/tokenService.ts
@@ -1,46 +1,41 @@
-import { ethers } from 'ethers'
-import { config } from '../config/config'
-import { communityAbi } from '../constants/community'
+import { ethers } from 'ethers';
+import { config } from '../config/config';
+import { communityAbi } from '../constants/community';
 
 const tokenTransferService = async (amount: string, recipient: string) => {
-    try {
-        console.log("🚀 ~ tokenTransferService ~ config.providerUrl:", config.providerUrl)
-        // Connect to the network
-        const provider = new ethers.JsonRpcProvider(config.providerUrl)
-        
-        console.log('🚀 ~ provider:', provider)
-        if (!provider) throw new Error('No provider found')
+	try {
+		// Connect to the network
+		const provider = new ethers.JsonRpcProvider(config.providerUrl);
 
-        // Check if contract address is valid
+		if (!provider) throw new Error('No provider found');
 
-        // Create a wallet instance
-        const wallet = new ethers.Wallet(config.privateKey, provider)
-        console.log('🚀 ~ wallet:', wallet)
+		// Check if contract address is valid
 
-        const contract = new ethers.Contract(
-            config.contractAddress,
-            communityAbi,
-            wallet
-        )
+		// Create a wallet instance
+		const wallet = new ethers.Wallet(config.privateKey, provider);
 
-        // Convert amount to proper units (assuming 18 decimals)
-        const decimals = 18
-        const parsedAmount = ethers.parseUnits(amount, decimals)
-        console.log('🚀 ~ parsedAmount:', parsedAmount)
+		const contract = new ethers.Contract(
+			config.contractAddress,
+			communityAbi,
+			wallet
+		);
 
-        // Send the transaction
-        const tx = await contract.withdrawTokens(recipient, parsedAmount)
-        console.log('🚀 ~ tx:', tx)
+		// Convert amount to proper units (assuming 18 decimals)
+		const decimals = 18;
+		const parsedAmount = ethers.parseUnits(amount, decimals);
 
-        // Wait for transaction confirmation
-        await tx.wait(3)
+		// Send the transaction
+		const tx = await contract.withdrawTokens(recipient, parsedAmount);
 
-        console.log(`Transaction successful: ${tx.hash}`)
-        return tx.hash
-    } catch (error) {
-        console.error('Transfer failed', error)
-        throw error
-    }
-}
+		// Wait for transaction confirmation
+		await tx.wait(3);
 
-export { tokenTransferService }
+		console.log(`Transaction successful: ${tx.hash}`);
+		return tx.hash;
+	} catch (error) {
+		console.error('Transfer failed', error);
+		throw error;
+	}
+};
+
+export { tokenTransferService };
